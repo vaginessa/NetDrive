973ea7cf13dcbbcaa72c9284f56cc06da4e2e727
engss <sweesineng@gmail.com>
2017-10-12 13:15:24 +0800

    1. 'setDrawerListener(android.support.v4.widget.DrawerLayout.DrawerListener)' is deprecated, change to 'addDrawerListener()', 2. add @Nonnull for MenuItem

--- app/src/main/java/com/homenas/netdrive/MainActivity.java
+++ app/src/main/java/com/homenas/netdrive/MainActivity.java
@@ -1,6 +1,7 @@
 package com.homenas.netdrive;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -90,7 +91,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
